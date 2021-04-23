fa974e062e16bb1a3d2c65064d599018d467fae4
Thomas Temple <tjt_84@hotmail.com>
2021-04-23 15:07:51 +1000

    added modal for lumina

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
