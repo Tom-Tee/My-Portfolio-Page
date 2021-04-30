c199f697ec97f256db6e147404e37d5f199090fc
Thomas Temple <tjt_84@hotmail.com>
2021-04-30 14:26:30 +1000

    updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,8 @@ class PagesController < ApplicationController
     send_file 'public/Portfolio.fig', type: 'image/png', status: 202
   end
 
+  def download_cv
+    send_file 'public/ThomasTempleCV.pdf', type: 'image/png', status: 202
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get '/download', to: "pages#download"
+  get '/download_cv', to: "pages#download_cv"
 end
